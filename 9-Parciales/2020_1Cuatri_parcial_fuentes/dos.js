cb3cf6bb9e4147ba66aa4c45e2214c6774daec9d
LudmilaMagri <ludmila.magri@gmail.com>
2021-03-01 12:46:44 -0300

    domingo

--- 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
@@ -1,4 +1,132 @@
+/*Realizar el algoritmo que permita ingresar los datos de una compra productos de la construccion, 
+hasta que el cliente quiera:
+Tipo validad("arena";"cal";"cemento")
+Cantidad de bolsas,
+Precio por bolsa (más de cero ),
+
+Si compro más de 10 bolsas en total tenes 15% de descuento sobre el total a pagar.
+Si compro más de 30 bolsas en total tenes 25% de descuento sobre el total a pagar.
+a) El importe total a pagar , bruto sin descuento y...
+b) el importe total a pagar con descuento(solo si corresponde)
+d) Informar el tipo con mas cantidad de bolsas.
+f) El tipo mas caro */
 function mostrar()
 {
-  alert("dos");
+
+	var seguir;
+	var tipo;
+	var cantidadBolsa;
+	var precioBolsa;
+
+	var descuentoTotalBolsa;
+	var descuentoPorDiezBolsas= 15;
+	var descuentoPorTreintaBolsas= 25;
+
+	var importeTotal;
+
+	var acumuladorPrecioBolsa=0;
+
+	var contadorArena=0;
+	var contadorCal=0;
+	var contadorCemento=0;
+
+	var acumuladorCantidadArena=0;
+	var acumuladorCantidadCemento=0;
+	var acumuladorCantidadCal=0;
+
+  do
+  {	
+  	tipo=prompt("Ingrese el tipo de bolsa: arena, cal, cemento");
+
+  	while(tipo!="arena" && tipo!= "cal" && tipo!= "cemento")
+  	{
+  		tipo=prompt("Ingrese nuevamente el tipo de bolsa: arena, cal, cemento");
+  	}
+  	cantidadBolsa=prompt("Ingrese la cantidad de bolsas");
+
+  	precioBolsa= prompt("Ingrese el precio de la bolsa");
+
+  	while(precioBolsa<0)
+  	{
+  		precioBolsa= prompt("Ingrese nuevamente el precio de la bolsa");
+  	}
+
+
+
+ /* 	Si compro más de 10 bolsas en total tenes 15% de descuento sobre el total a pagar.
+Si compro más de 30 bolsas en total tenes 25% de descuento sobre el total a pagar.
+a) El importe total a pagar , bruto sin descuento y...
+b) el importe total a pagar con descuento(solo si corresponde)
+d) Informar el tipo con mas cantidad de bolsas.
+f) El tipo mas caro */
+
+	if(cantidadBolsa>30)
+	{
+		descuentoTotalBolsa= (precioBolsa*descuentoPorTreintaBolsas/100)-precioBolsa;
+	}
+
+	else 
+	{
+		if(cantidadBolsa>10 && cantidadBolsa<30)
+		{
+			descuentoTotalBolsa= (precioBolsa*descuentoPorDiezBolsas/100)-precioBolsa;
+			console.log("Su total a pagar con descuento es: "+descuentoTotalBolsa);
+		}
+		
+
+		else 
+		{
+			importeTotal= acumuladorPrecioBolsa;
+			console.log ("Su total a pagar es: "+ importeTotal);
+		}
+		
+
+
+	switch(tipo)
+	{
+		case ("arena"):
+			contadorArena++;
+			acumuladorCantidadArena=acumuladorCantidadArena+cantidadBolsa;
+		break;
+
+		case ("cal"):
+			contadorCal++;
+			acumuladorCantidadCal=acumuladorCantidadCal+cantidadBolsa
+		break;
+
+		case ("cemento"):
+			contadorCemento++;
+			acumuladorCantidadCemento=acumuladorCantidadCemento+cantidadBolsa;
+		break;
+
+
+	}
+		if(acumuladorCantidadArena>acumuladorCantidadCemento && acumuladorCantidadArena> acumuladorCantidadCal)
+		{
+				console.log("Hay mas bolsas de arena");
+
+		}
+		else
+		{
+			if(acumuladorCantidadCemento>acumuladorCantidadCal)
+			{
+				console.log ("Hay mas bolsas de cemento");
+			}
+
+			else 
+			{
+				console.log ("Hay mas bolsas de Cal");
+			}
+		}
+
+
+	acumuladorPrecioBolsa=acumuladorPrecioBolsa+precio;
+
+  	seguir= confirm("Desea ingresar otro producto? (si/no)");
+
+  } while(seguir);
+
+
+ 	
+
 }
--- 9-Parciales/2020_1Cuatri_parcial_fuentes/uno.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/uno.js
@@ -17,28 +17,45 @@ function mostrar()
 	var marca;
 	var fabricante;
 	var respuesta;
-	
+	var flag=true;
+
+	var precioMin;
+	var fabricanteMasBaratoAlcohol;
+	var cantidadMasBaratoAlcohol;
+
+	var contadorJabon=0;
+	var contadorBarbijo=0;
+	var contadorAlcohol=0;
+
+	var acumuladorJabon=0;
+	var acumuladorBarbijo=0;
+	var acumuladorAlcohol=0;
+
 	var contadorProducto=0;
 
-	while(contadorProducto<5)//hacer con for o while <5
+	for(var i=0 ; i<5; i++)//hacer con for o while <5
 	{
-		tipo=prompt("Ingrese el tipo del producto: (barbijo/ jabon/ alcohol");
+		tipo =prompt("Ingrese el tipo del producto: (barbijo/ jabon/ alcohol");
+		console.log(tipo);
 		
-		while(tipo!= "barbijo" && tipo!="jabon" && tipo!="alcohol")
+		while(tipo != "barbijo" && tipo != "jabon" && tipo !="alcohol")
 		{
 			tipo=prompt("Ingrese nuevamente el tipo del producto: (barbijo/ jabon/ alcohol");
 			console.log(tipo);
 		}
 
-		precio=prompt("Ingrese el precio del producto");
+		precio=parseInt(prompt("Ingrese el precio del producto"));
+		console.log(precio);
+
 		while(precio<100 || precio>300)
 		{
-			precio=prompt("Ingrese nuevamente el precio del producto");
+			precio=parseInt(prompt("Ingrese nuevamente el precio del producto"));
 			console.log(precio);	
 			
 		}
-		cantidad=prompt("Ingrese la cantidad del producto");
-		
+		cantidad=parseInt(prompt("Ingrese la cantidad del producto"));
+		console.log(cantidad);
+
 		while(cantidad<=0 || cantidad>1000)
 		{
 			cantidad=prompt("Ingrese nuevamente la cantidad del producto");
@@ -49,8 +66,74 @@ function mostrar()
 
 		fabricante=prompt("Ingrese el fabricante del producto");
 		console.log(fabricante);
+
+
+		contadorProducto++;
+	
+
+		switch (tipo)
+		{
+			case "alcohol":
+				contadorAlcohol++;
+				acumuladorAlcohol=acumuladorAlcohol+contadorAlcohol;
+
+				if (flag==true)
+				{
+					precioMin=precio;
+					cantidadMasBaratoAlcohol=cantidad;
+					fabricanteMasBaratoAlcohol=fabricante;
+					flag=false;
+					
+				}
+				else if(precio<precioMin)
+				{
+					precioMin=precio;
+					cantidadMasBaratoAlcohol=cantidad;
+					fabricanteMasBaratoAlcohol=fabricante;
+				}
+				break;
+
+			case "barbijo":
+				contadorBarbijo++;
+				acumuladorBarbijo=acumuladorBarbijo+contadorBarbijo;
+				break;
+
+			case "jabon":
+				contadorJabon++;
+				acumuladorJabon=acumuladorJabon+contadorJabon;
+
+				break;
+				
+
+			}
+		if (acumuladorJabon>acumuladorBarbijo && acumuladorJabon>acumuladorAlcohol)
+			{
+				console.log ("Hay mas jabones");
+			}
+			else
+			{
+				if(acumuladorBarbijo>acumuladorAlcohol)
+				{
+				console.log ("Hay mas barbijos");
+				}
+				else
+				{
+					console.log ("Hay mas alcohol");
+				}
+
+		}
+		
+
 	}
+
+	//a) Del más barato de los alcohol, la cantidad de unidades y el fabricante
+	console.log("La cantidad y el fabricante del alcohol mas barato: " + cantidadMasBaratoAlcohol+" "+ fabricanteMasBaratoAlcohol);
+ 	//b) Del tipo con mas unidades, el promedio por compra
+ 	console.log ("El tipo de producto con mas unidades: ");
+	//c) Cuántas unidades de jabones hay en total
+	console.log("Total de jabones:" +contadorJabon);
 			
-
-
 }
+
+
+	
